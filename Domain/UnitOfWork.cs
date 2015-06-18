4b8bcff6f252264c587dca6b3cf70114632bb6a0
Roma <romamarusyk@gmail.com>
2015-06-19 01:14:02 +0300

    Add UnitOfWork

---
+++ Domain/UnitOfWork.cs
@@ -0,0 +1,68 @@
+﻿using Domain.Abstract;
+using Domain.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Domain
+{
+    public class UnitOfWork : IDisposable
+    {
+        private readonly EFDbContext context;
+        private bool disposed;
+        private Dictionary<string, object> repositories;
+
+        public UnitOfWork(EFDbContext context)
+        {
+            this.context = context;
+        }
+
+        public UnitOfWork()
+        {
+            context = new EFDbContext();
+        }
+
+        public void Dispose()
+        {
+            Dispose(true);
+            GC.SuppressFinalize(this);
+        }
+
+        public void Save()
+        {
+            context.SaveChanges();
+        }
+
+        public virtual void Dispose(bool disposing)
+        {
+            if (!disposed)
+            {
+                if (disposing)
+                {
+                    context.Dispose();
+                }
+            }
+            disposed = true;
+        }
+
+        public EFRepository<T> EFRepository<T>() where T : BaseEntity
+        {
+            if (repositories == null)
+            {
+                repositories = new Dictionary<string, object>();
+            }
+
+            var type = typeof(T).Name;
+
+            if (!repositories.ContainsKey(type))
+            {
+                var repositoryType = typeof(EFRepository<>);
+                var repositoryInstance = Activator.CreateInstance(repositoryType.MakeGenericType(typeof(T)), context);
+                repositories.Add(type, repositoryInstance);
+            }
+            return (EFRepository<T>)repositories[type];
+        }
+    }
+}
