1f4935613225b0fe5383e9182ae081f37966fea1
Roma <romamarusyk@gmail.com>
2015-06-20 01:51:23 +0300

    Set JSON as return content

--- WebUI/App_Start/WebApiConfig.cs
+++ WebUI/App_Start/WebApiConfig.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net.Http.Headers;
 using System.Web.Http;
 
 namespace WebUI
@@ -28,6 +29,9 @@ namespace WebUI
             // To disable tracing in your application, please comment out or remove the following line of code
             // For more information, refer to: http://www.asp.net/web-api
             config.EnableSystemDiagnosticsTracing();
+
+            // return JSON
+            config.Formatters.JsonFormatter.SupportedMediaTypes.Add(new MediaTypeHeaderValue("text/html"));
         }
     }
 }
