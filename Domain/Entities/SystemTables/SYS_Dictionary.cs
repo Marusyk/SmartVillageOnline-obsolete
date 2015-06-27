12c7355601275f33371f922c30b8e5d979e44348
Roma <romamarusyk@gmail.com>
2015-06-27 19:42:24 +0300

    Folder for system tables. Added entity to list of dictionary

---
+++ Domain/Entities/SystemTables/SYS_Dictionary.cs
@@ -0,0 +1,11 @@
+﻿using Domain.Abstract;
+
+namespace Domain.Entities.SystemTables
+{
+    public class SYS_Dictionary : BaseEntity
+    {
+        public string Name { get { return Name; } }
+        public string Description { get { return Description; } }
+        public bool IsStatic { get { return IsStatic; } }
+    }
+}
---
+++ Domain/Mapping/SystemTables/DictionaryMap.cs
@@ -0,0 +1,19 @@
+﻿using Domain.Entities.SystemTables;
+using System.Data.Entity.ModelConfiguration;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Domain.Mapping.SystemTables
+{
+    public class DictionaryMap : EntityTypeConfiguration<SYS_Dictionary>
+    {
+        public DictionaryMap()
+        {
+            HasKey(t => t.ID);
+            Property(t => t.ID).HasDatabaseGeneratedOption(DatabaseGeneratedOption.Identity);
+            Property(t => t.Name).IsRequired();
+            Property(t => t.Description).IsRequired();
+            Property(t => t.IsStatic).IsRequired();
+            ToTable("SYS_Dictionary");
+        }
+    }
+}
