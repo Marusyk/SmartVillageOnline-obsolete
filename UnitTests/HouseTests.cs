5dfa9107e72476643db1b9e8f52fbfe03daa0203
Roma <romamarusyk@gmail.com>
2015-06-28 17:25:51 +0300

    Unit tests for House

---
+++ UnitTests/HouseTests.cs
@@ -0,0 +1,152 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Domain.Abstract;
+using Domain.Entities;
+using Moq;
+using System.Collections.Generic;
+using System.Linq;
+using WebUI.Controllers.API;
+using System.Net.Http;
+using System.Web.Http.Controllers;
+using System.Web.Http.Hosting;
+using System.Web.Http;
+using System.Net;
+
+namespace UnitTests
+{
+    [TestClass]
+    public class HouseTests
+    {
+        #region private
+
+        private readonly string MainUri = "http://localhost/api/house";
+
+        private IRepository<House> CreateMockRepository()
+        {
+            // creating  fake repository
+            var houses = new List<House>
+            {
+                new House {ID =1, HouseNr ="001", BuildNr ="1"},
+                new House {ID =2, HouseNr ="002", BuildNr= "2" },
+                new House {ID =3, HouseNr ="003", BuildNr ="3" },
+                new House {ID =4, HouseNr ="004", BuildNr ="4"},
+                new House {ID =5, HouseNr ="005", BuildNr ="5" }
+            };
+
+            // configure the Mock Object
+            Mock<IRepository<House>> mock = new Mock<IRepository<House>>();
+
+            mock.Setup(m => m.Table).Returns(houses.AsQueryable());
+
+            mock.Setup(m => m.Insert(It.IsAny<House>()))
+                .Callback<House>(c => houses.Add(c));
+
+            mock.Setup(m => m.Update(It.IsAny<House>()))
+                .Callback<House>(c => houses[houses.IndexOf(c)] = c);
+
+            mock.Setup(m => m.GetById(It.IsAny<int>()))
+                .Returns<int>(c => houses.Find(f => f.ID == c));
+
+            mock.Setup(m => m.Delete(It.IsAny<House>()))
+                .Callback<House>(c => houses.Remove(c));
+
+            return mock.Object;
+        }
+
+        #endregion
+
+        [TestMethod]
+        public void Get_All_Houses()
+        {
+            //Arrange - create mock repository               
+            var moq = CreateMockRepository();
+
+            //Arrange - create a controller
+            HouseController target = new HouseController(moq);
+
+            //Action
+            var result = target.Get().ToArray();
+
+            //Assert
+            Assert.AreEqual(5, result.Length);
+        }
+
+        [TestMethod]
+        public void Can_Insert_House()
+        {
+            //Arrange - get the mock repository               
+            var moq = CreateMockRepository();
+
+            //Arrange - create and configure controller            
+            var request = new HttpRequestMessage(HttpMethod.Post, MainUri);
+
+            HouseController target = new HouseController(moq);
+
+            target.ControllerContext = new HttpControllerContext() { Request = request };
+            target.Request = request;
+            target.Request.Properties[HttpPropertyKeys.HttpConfigurationKey] = new HttpConfiguration();
+
+            //Arrange - create a new country for insert
+            House newHouse = new House() { ID = 10, BuildNr = "010", HouseNr = "10" };
+
+            //Action
+            var resultInsert = target.Post(newHouse);
+            var resultSelect = target.Get().ToArray();
+
+            //Assert
+            Assert.AreEqual(6, resultSelect.Length);
+            Assert.AreEqual(HttpStatusCode.Created, resultInsert.StatusCode);
+        }
+
+        [TestMethod]
+        public void Can_Edit_House()
+        {
+            //Arrange - get the mock repository
+            var moq = CreateMockRepository();
+
+            //Arrange - create and configure controller                        
+            var request = new HttpRequestMessage(HttpMethod.Put, MainUri);
+
+            HouseController target = new HouseController(moq);
+
+            target.ControllerContext = new HttpControllerContext() { Request = request };
+            target.Request = request;
+            target.Request.Properties[HttpPropertyKeys.HttpConfigurationKey] = new HttpConfiguration(); ;
+
+            //Action                     
+            var house = target.GetById(1);
+            house.HouseNr = "007";
+            var resultUpdate = target.Put(house);
+            var resultSelect = target.Get().ToArray();
+
+            //Assert
+            Assert.AreEqual(HttpStatusCode.OK, resultUpdate.StatusCode);
+            Assert.AreEqual("007", resultSelect[0].HouseNr);
+        }
+
+        [TestMethod]
+        public void Can_Remove_House()
+        {
+            //Arrange - get the mock repository
+            var moq = CreateMockRepository();
+
+            //Arrange - create and configure controller                      
+            var request = new HttpRequestMessage(HttpMethod.Delete, MainUri);
+
+            HouseController target = new HouseController(moq);
+
+            target.ControllerContext = new HttpControllerContext() { Request = request };
+            target.Request = request;
+            target.Request.Properties[HttpPropertyKeys.HttpConfigurationKey] = new HttpConfiguration(); ;
+
+            //Action
+            var house = target.GetById(1);
+            var resultDelete = target.Delete(house.ID);
+            var resultSelect = target.Get().ToArray();
+
+            //Assert
+            Assert.AreEqual(HttpStatusCode.OK, resultDelete.StatusCode);
+            Assert.AreEqual(4, resultSelect.Length);
+        }
+    }
+}
---
+++ WebUI/Controllers/API/ApiBaseController.cs
@@ -0,0 +1,103 @@
+﻿using Domain;
+using Domain.Abstract;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace WebUI.Controllers.API
+{
+    public abstract class ApiBaseController<T> : ApiController where T : BaseEntity
+    {
+        protected UnitOfWork unitOfWork = new UnitOfWork();
+
+        protected IRepository<T> repository;
+
+        public ApiBaseController()
+        {
+            repository = unitOfWork.EFRepository<T>();
+        }
+
+        public ApiBaseController(IRepository<T> repository)
+        {
+            this.repository = repository;
+        }
+
+        public virtual IEnumerable<T> Get()
+        {
+            return repository.Table.ToList();
+        }
+
+        public virtual T GetById(int id)
+        {
+            var entity = repository.GetById(id);
+            if (entity == null)
+            {
+                throw new HttpResponseException(HttpStatusCode.NotFound);
+            }
+            return entity;
+        }
+
+        public virtual HttpResponseMessage Post([FromBody]T entity)
+        {
+            try
+            {
+                repository.Insert(entity);
+                return Request.CreateResponse(HttpStatusCode.Created, entity);
+            }
+            catch (Exception)
+            {
+                return Request.CreateResponse(HttpStatusCode.InternalServerError);
+            }
+
+        }
+
+        public virtual HttpResponseMessage Delete(int id)
+        {
+            T toDelete = repository.GetById(id);
+
+            if (toDelete == null)
+            {
+                return Request.CreateResponse(HttpStatusCode.NotFound);
+            }
+            try
+            {
+                repository.Delete(toDelete);
+                return Request.CreateResponse(HttpStatusCode.OK);
+            }
+            catch (Exception)
+            {
+                return Request.CreateResponse(HttpStatusCode.InternalServerError);
+            }
+        }
+
+        public virtual HttpResponseMessage Put([FromBody]T entity)
+        {
+            T oldEntity = repository.GetById(entity.ID);
+
+            if (oldEntity == null)
+            {
+                return Request.CreateResponse(HttpStatusCode.NotFound);
+            }
+            try
+            {
+                oldEntity = entity;
+                oldEntity.LastUpdDT = DateTime.Now;
+                repository.Update(oldEntity);
+                return Request.CreateResponse(HttpStatusCode.OK, oldEntity);
+            }
+            catch (Exception)
+            {
+                return Request.CreateResponse(HttpStatusCode.InternalServerError);
+            }
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            unitOfWork.Dispose();
+            base.Dispose(disposing);
+        }
+    }
+}
