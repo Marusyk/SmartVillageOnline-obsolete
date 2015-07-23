d80fc1ab78949d05a53865381547804136be7f91
Roma <romamarusyk@gmail.com>
2015-07-24 00:29:10 +0300

    Materials controller and unit tests. Issue #3

---
+++ WebUI/Controllers/API/MaterialsController.cs
@@ -0,0 +1,19 @@
+﻿using Domain.Entities;
+using Domain.Abstract;
+
+namespace WebUI.Controllers.API
+{
+    public class MaterialsController : BaseApiController<Materials>
+    {
+        public MaterialsController()
+            : base()
+        {
+        }
+
+        public MaterialsController(IRepository<Materials> repository)
+            : base(repository)
+        {
+            this.repository = repository;
+        }
+    }
+}
