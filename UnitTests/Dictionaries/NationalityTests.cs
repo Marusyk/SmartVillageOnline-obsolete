57cd33a2d82fbe940ef648364ae8421050be28bd
Roma <romamarusyk@gmail.com>
2015-07-24 00:43:53 +0300

    Nationality controller and unit tests. Issue #3

---
+++ UnitTests/Dictionaries/NationalityTests.cs
@@ -0,0 +1,54 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Domain.Entities;
+using WebUI.Controllers.API;
+
+namespace UnitTests.Dictionaries
+{
+    [TestClass]
+    public class NationalityTests : BaseDictionaryTests<Nationality>
+    {
+        public NationalityTests()
+            : base()
+        {
+            // get Mock repository from base class
+            var moq = base.CreateMockRepository();
+
+            // create controller with Mock
+            var controller = new NationalityController(moq);
+
+            // Init params of controller
+            base.ArrangeController(controller);
+        }
+
+
+        [TestMethod]
+        public void Nationality_Get_All()
+        {
+            base.GetAll();
+        }
+
+        [TestMethod]
+        public void Nationality_Get_By_Id()
+        {
+            base.GetById();
+        }
+
+        [TestMethod]
+        public void Nationality_Can_Insert()
+        {
+            base.Insert();
+        }
+
+        [TestMethod]
+        public void Nationality_Can_Edit()
+        {
+            base.Edit();
+        }
+
+        [TestMethod]
+        public void Nationality_Can_Remove()
+        {
+            base.Remove();
+        }
+    }
+}
---
+++ WebUI/Controllers/API/NationalityController.cs
@@ -0,0 +1,19 @@
+﻿using Domain.Entities;
+using Domain.Abstract;
+
+namespace WebUI.Controllers.API
+{
+    public class NationalityController : BaseApiController<Nationality>
+    {
+        public NationalityController()
+            : base()
+        {
+        }
+
+        public NationalityController(IRepository<Nationality> repository)
+            : base(repository)
+        {
+            this.repository = repository;
+        }
+    }
+}
