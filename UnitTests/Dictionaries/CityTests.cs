b2554c8a103b01f3f6279208f5c40419c6e417db
Roma <romamarusyk@gmail.com>
2015-07-23 22:01:12 +0300

    City controller and unit tests. #3

---
+++ UnitTests/Dictionaries/CityTests.cs
@@ -0,0 +1,53 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using WebUI.Controllers.API;
+using Domain.Entities.Dictionaries;
+
+namespace UnitTests.Dictionaries
+{
+    [TestClass]
+    class CityTests : BaseDictionaryTests<City>
+    {
+        public CityTests()
+            : base()
+        {
+            // get Mock repository from base class
+            var moq = base.CreateMockRepository();
+
+            // create controller with Mock
+            var controller = new CityController(moq);
+
+            // Init params of controller
+            base.ArrangeController(controller);
+        }
+
+        [TestMethod]
+        public void City_Get_All()
+        {
+            base.GetAll();
+        }
+
+        [TestMethod]
+        public void City_Get_By_Id()
+        {
+            base.GetById();
+        }
+
+        [TestMethod]
+        public void City_Can_Insert()
+        {
+            base.Insert();
+        }
+
+        [TestMethod]
+        public void City_Can_Edit()
+        {
+            base.Edit();
+        }
+
+        [TestMethod]
+        public void City_Can_Remove()
+        {
+            base.Remove();
+        }
+    }
+}
---
+++ WebUI/Controllers/API/CityController.cs
@@ -0,0 +1,19 @@
+﻿using Domain.Abstract;
+using Domain.Entities.Dictionaries;
+
+namespace WebUI.Controllers.API
+{
+    public class CityController : BaseApiController<City>
+    {
+        public CityController()
+            : base()
+        {
+        }
+
+        public CityController(IRepository<City> repository)
+            : base(repository)
+        {
+            this.repository = repository;
+        }
+    }
+}
