e89c3dd978989855e9a9c0eac788ca6772b72be9
Roma <romamarusyk@gmail.com>
2015-07-24 00:25:12 +0300

    FamilyStatus controller and unit tests. Issue #3

---
+++ WebUI/Controllers/API/FamilyStatusController.cs
@@ -0,0 +1,19 @@
+﻿using Domain.Entities;
+using Domain.Abstract;
+
+namespace WebUI.Controllers.API
+{
+    public class FamilyStatusController : BaseApiController<FamilyStatus>
+    {
+        public FamilyStatusController()
+            : base()
+        {
+        }
+
+        public FamilyStatusController(IRepository<FamilyStatus> repository)
+            : base(repository)
+        {
+            this.repository = repository;
+        }
+    }
+}
