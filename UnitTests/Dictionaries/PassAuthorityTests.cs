9fadbfc6b15a6e32a2067fdaf4deaeebe39b53e1
Roma <romamarusyk@gmail.com>
2015-07-24 00:45:05 +0300

    PassAuthorityMap controller and unit tests. Issue #3

---
+++ UnitTests/Dictionaries/PassAuthorityTests.cs
@@ -0,0 +1,54 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Domain.Entities;
+using WebUI.Controllers.API;
+
+namespace UnitTests.Dictionaries
+{
+    [TestClass]
+    public class PassAuthorityTests : BaseDictionaryTests<PassAuthority>
+    {
+        public PassAuthorityTests()
+            : base()
+        {
+            // get Mock repository from base class
+            var moq = base.CreateMockRepository();
+
+            // create controller with Mock
+            var controller = new PassAuthorityController(moq);
+
+            // Init params of controller
+            base.ArrangeController(controller);
+        }
+
+
+        [TestMethod]
+        public void PassAuthority_Get_All()
+        {
+            base.GetAll();
+        }
+
+        [TestMethod]
+        public void PassAuthority_Get_By_Id()
+        {
+            base.GetById();
+        }
+
+        [TestMethod]
+        public void PassAuthority_Can_Insert()
+        {
+            base.Insert();
+        }
+
+        [TestMethod]
+        public void PassAuthority_Can_Edit()
+        {
+            base.Edit();
+        }
+
+        [TestMethod]
+        public void PassAuthority_Can_Remove()
+        {
+            base.Remove();
+        }
+    }
+}
---
+++ WebUI/Controllers/API/PassAuthorityController.cs
@@ -0,0 +1,19 @@
+﻿using Domain.Entities;
+using Domain.Abstract;
+
+namespace WebUI.Controllers.API
+{
+    public class PassAuthorityController : BaseApiController<PassAuthority>
+    {
+        public PassAuthorityController()
+            : base()
+        {
+        }
+
+        public PassAuthorityController(IRepository<PassAuthority> repository)
+            : base(repository)
+        {
+            this.repository = repository;
+        }
+    }
+}
