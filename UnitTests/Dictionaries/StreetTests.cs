b241dd325113e8dbccb6bcfaf62ac8984ed2f0ed
Roma <romamarusyk@gmail.com>
2015-07-23 21:53:10 +0300

    Unit tests for Street

---
+++ UnitTests/Dictionaries/StreetTests.cs
@@ -0,0 +1,53 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using WebUI.Controllers.API;
+using Domain.Entities;
+
+namespace UnitTests.Dictionaries
+{
+    [TestClass]
+    public class StreetTests : BaseDictionaryTests<Street>
+    {
+        public StreetTests()
+            : base()
+        {
+            // get Mock repository from base class
+            var moq = base.CreateMockRepository();
+
+            // create controller with Mock
+            var controller = new StreetController(moq);
+
+            // Init params of controller
+            base.ArrangeController(controller);
+        }
+
+        [TestMethod]
+        public void Street_Get_All()
+        {
+            base.GetAll();
+        }
+
+        [TestMethod]
+        public void Street_Get_By_Id()
+        {
+            base.GetById();
+        }
+
+        [TestMethod]
+        public void Street_Can_Insert()
+        {
+            base.Insert();
+        }
+
+        [TestMethod]
+        public void Street_Can_Edit()
+        {
+            base.Edit();
+        }
+
+        [TestMethod]
+        public void Street_Can_Remove()
+        {
+            base.Remove();
+        }
+    }
+}
