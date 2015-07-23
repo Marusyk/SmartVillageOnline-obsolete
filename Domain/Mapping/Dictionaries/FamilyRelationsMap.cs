e8c6bcd76be663f143f4b8d824f3d576319faef9
Roma <romamarusyk@gmail.com>
2015-07-24 00:20:55 +0300

    FamilyRelations controller and unit tests. Issue #3

---
+++ Domain/Mapping/Dictionaries/FamilyRelationsMap.cs
@@ -0,0 +1,18 @@
+﻿using System.Data.Entity.ModelConfiguration;
+using Domain.Entities;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Domain.Mapping.Dictionaries
+{
+    public class FamilyRelationsMap : EntityTypeConfiguration<FamilyRelations>
+    {
+        public FamilyRelationsMap()
+        {
+            HasKey(t => t.ID);
+            Property(t => t.ID).HasDatabaseGeneratedOption(DatabaseGeneratedOption.Identity);
+            Property(t => t.Name).IsRequired().HasMaxLength(50);
+            Property(t => t.LastUpdUS).IsRequired().HasMaxLength(50);
+            ToTable("FamilyRelations");
+        }
+    }
+}
---
+++ WebUI/Controllers/API/FamilyRelationsController.cs
@@ -0,0 +1,19 @@
+﻿using Domain.Entities;
+using Domain.Abstract;
+
+namespace WebUI.Controllers.API
+{
+    public class FamilyRelationsController : BaseApiController<FamilyRelations>
+    {
+        public FamilyRelationsController()
+            : base()
+        {
+        }
+
+        public FamilyRelationsController(IRepository<FamilyRelations> repository)
+            : base(repository)
+        {
+            this.repository = repository;
+        }
+    }
+}
