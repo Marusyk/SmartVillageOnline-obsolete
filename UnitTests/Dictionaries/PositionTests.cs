08865441404fa2ade80657e4781b35aaf2a29fd5
Roma <romamarusyk@gmail.com>
2015-07-24 00:46:58 +0300

    PositionMap controller and unit tests. Issue #3

---
+++ UnitTests/Dictionaries/PositionTests.cs
@@ -0,0 +1,54 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Domain.Entities;
+using WebUI.Controllers.API;
+
+namespace UnitTests.Dictionaries
+{
+    [TestClass]
+    public class PositionTests : BaseDictionaryTests<Position>
+    {
+        public PositionTests()
+            : base()
+        {
+            // get Mock repository from base class
+            var moq = base.CreateMockRepository();
+
+            // create controller with Mock
+            var controller = new PositionController(moq);
+
+            // Init params of controller
+            base.ArrangeController(controller);
+        }
+
+
+        [TestMethod]
+        public void Position_Get_All()
+        {
+            base.GetAll();
+        }
+
+        [TestMethod]
+        public void Position_Get_By_Id()
+        {
+            base.GetById();
+        }
+
+        [TestMethod]
+        public void Position_Can_Insert()
+        {
+            base.Insert();
+        }
+
+        [TestMethod]
+        public void Position_Can_Edit()
+        {
+            base.Edit();
+        }
+
+        [TestMethod]
+        public void Position_Can_Remove()
+        {
+            base.Remove();
+        }
+    }
+}
---
+++ WebUI/Controllers/API/PositionController.cs
@@ -0,0 +1,19 @@
+﻿using Domain.Entities;
+using Domain.Abstract;
+
+namespace WebUI.Controllers.API
+{
+    public class PositionController : BaseApiController<Position>
+    {
+        public PositionController()
+            : base()
+        {
+        }
+
+        public PositionController(IRepository<Position> repository)
+            : base(repository)
+        {
+            this.repository = repository;
+        }
+    }
+}
