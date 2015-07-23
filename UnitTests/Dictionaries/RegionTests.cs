12c17e157d20fb4b4e82a893b085144cbdc0c4e7
Roma <romamarusyk@gmail.com>
2015-07-23 21:20:35 +0300

    Region controller and unit tests. #3

---
+++ UnitTests/Dictionaries/RegionTests.cs
@@ -0,0 +1,55 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Domain.Entities;
+using WebUI.Controllers.API;
+using UnitTests.Dictionaries;
+using Domain.Entities.Dictionaries;
+
+namespace UnitTests.Dictionaries
+{
+    [TestClass]
+    public class RegionTests : BaseDictionaryTests<Region>
+    {
+        public RegionTests()
+            : base()
+        {
+            // get Mock repository from base class
+            var moq = base.CreateMockRepository();
+
+            // create controller with Mock
+            var controller = new RegionController(moq);
+
+            // Init params of controller
+            base.ArrangeController(controller);
+        }
+
+        [TestMethod]
+        public void Region_Get_All()
+        {
+            base.GetAll();
+        }
+
+        [TestMethod]
+        public void Region_Get_By_Id()
+        {
+            base.GetById();
+        }
+
+        [TestMethod]
+        public void Region_Can_Insert()
+        {
+            base.Insert();
+        }
+
+        [TestMethod]
+        public void Region_Can_Edit()
+        {
+            base.Edit();
+        }
+
+        [TestMethod]
+        public void Region_Can_Remove()
+        {
+            base.Remove();
+        }
+    }
+}
---
+++ WebUI/Controllers/API/RegionController.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using Domain.Entities.Dictionaries;
+using Domain.Abstract;
+
+namespace WebUI.Controllers.API
+{
+    public class RegionController : BaseApiController<Region>
+    {
+        public RegionController()
+            : base()
+        {
+        }
+
+        public RegionController(IRepository<Region> repository)
+            : base(repository)
+        {
+            this.repository = repository;
+        }
+    }
+}
