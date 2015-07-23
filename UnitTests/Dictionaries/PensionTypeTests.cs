83834de7027b20ee1d2655f3c4da42c1ddf79a99
Roma <romamarusyk@gmail.com>
2015-07-24 00:46:07 +0300

    PensionType controller and unit tests. Issue #3

---
+++ UnitTests/Dictionaries/PensionTypeTests.cs
@@ -0,0 +1,54 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Domain.Entities;
+using WebUI.Controllers.API;
+
+namespace UnitTests.Dictionaries
+{
+    [TestClass]
+    public class PensionTypeTests : BaseDictionaryTests<PensionType>
+    {
+        public PensionTypeTests()
+            : base()
+        {
+            // get Mock repository from base class
+            var moq = base.CreateMockRepository();
+
+            // create controller with Mock
+            var controller = new PensionTypeController(moq);
+
+            // Init params of controller
+            base.ArrangeController(controller);
+        }
+
+
+        [TestMethod]
+        public void PensionType_Get_All()
+        {
+            base.GetAll();
+        }
+
+        [TestMethod]
+        public void PensionType_Get_By_Id()
+        {
+            base.GetById();
+        }
+
+        [TestMethod]
+        public void PensionType_Can_Insert()
+        {
+            base.Insert();
+        }
+
+        [TestMethod]
+        public void PensionType_Can_Edit()
+        {
+            base.Edit();
+        }
+
+        [TestMethod]
+        public void PensionType_Can_Remove()
+        {
+            base.Remove();
+        }
+    }
+}
---
+++ WebUI/Controllers/API/PensionTypeController.cs
@@ -0,0 +1,19 @@
+﻿using Domain.Entities;
+using Domain.Abstract;
+
+namespace WebUI.Controllers.API
+{
+    public class PensionTypeController : BaseApiController<PensionType>
+    {
+        public PensionTypeController()
+            : base()
+        {
+        }
+
+        public PensionTypeController(IRepository<PensionType> repository)
+            : base(repository)
+        {
+            this.repository = repository;
+        }
+    }
+}
