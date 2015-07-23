087ae6fa0385f7de413e25cc775e5a86e7348691
Roma <romamarusyk@gmail.com>
2015-07-23 21:24:21 +0300

    Unit tests for StreetType

--- UnitTests/Dictionaries/StreetTypeTests.cs
+++ UnitTests/Dictionaries/StreetTypeTests.cs
@@ -1,51 +1,51 @@
 ﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
 using WebUI.Controllers.API;
-using Domain.Entities.Dictionaries;
+using Domain.Entities;
 
 namespace UnitTests.Dictionaries
 {
     [TestClass]
-    public class RegionTests : BaseDictionaryTests<Region>
+    public class StreetTypeTests : BaseDictionaryTests<StreetType>
     {
-        public RegionTests()
+        public StreetTypeTests()
             : base()
         {
             // get Mock repository from base class
             var moq = base.CreateMockRepository();
 
             // create controller with Mock
-            var controller = new RegionController(moq);
+            var controller = new StreetTypeController(moq);
 
             // Init params of controller
             base.ArrangeController(controller);
         }
 
         [TestMethod]
-        public void Region_Get_All()
+        public void StreetType_Get_All()
         {
             base.GetAll();
         }
 
         [TestMethod]
-        public void Region_Get_By_Id()
+        public void StreetType_Get_By_Id()
         {
             base.GetById();
         }
 
         [TestMethod]
-        public void Region_Can_Insert()
+        public void StreetType_Can_Insert()
         {
             base.Insert();
         }
 
         [TestMethod]
-        public void Region_Can_Edit()
+        public void StreetType_Can_Edit()
         {
             base.Edit();
         }
 
         [TestMethod]
-        public void Region_Can_Remove()
+        public void StreetType_Can_Remove()
         {
             base.Remove();
         }
