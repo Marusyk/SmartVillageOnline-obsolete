a8eebe7d12e1d34addc48faf5d988d243eead29f
Roma <romamarusyk@gmail.com>
2015-07-23 21:56:41 +0300

    District controller and unit tests. #3

---
+++ UnitTests/Dictionaries/DistrictTests.cs
@@ -0,0 +1,53 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using WebUI.Controllers.API;
+using Domain.Entities.Dictionaries;
+
+namespace UnitTests.Dictionaries
+{
+    [TestClass]
+    public class DistrictTests : BaseDictionaryTests<District>
+    {
+        public DistrictTests()
+            : base()
+        {
+            // get Mock repository from base class
+            var moq = base.CreateMockRepository();
+
+            // create controller with Mock
+            var controller = new DistrictController(moq);
+
+            // Init params of controller
+            base.ArrangeController(controller);
+        }
+
+        [TestMethod]
+        public void District_Get_All()
+        {
+            base.GetAll();
+        }
+
+        [TestMethod]
+        public void District_Get_By_Id()
+        {
+            base.GetById();
+        }
+
+        [TestMethod]
+        public void District_Can_Insert()
+        {
+            base.Insert();
+        }
+
+        [TestMethod]
+        public void District_Can_Edit()
+        {
+            base.Edit();
+        }
+
+        [TestMethod]
+        public void District_Can_Remove()
+        {
+            base.Remove();
+        }
+    }
+}
---
+++ WebUI/Controllers/API/DistrictController.cs
@@ -0,0 +1,19 @@
+﻿using Domain.Entities.Dictionaries;
+using Domain.Abstract;
+
+namespace WebUI.Controllers.API
+{
+    public class DistrictController : BaseApiController<District>
+    {
+        public DistrictController()
+            : base()
+        {
+        }
+
+        public DistrictController(IRepository<District> repository)
+            : base(repository)
+        {
+            this.repository = repository;
+        }
+    }
+}
