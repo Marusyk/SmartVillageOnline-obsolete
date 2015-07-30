59e1063be75a0730155a78c5644af084563b0912
Roma <romamarusyk@gmail.com>
2015-07-31 02:13:10 +0300

    Create JS-client

--- WebUI/Scripts/DictionaryTest.js
+++ WebUI/Scripts/DictionaryTest.js
@@ -1,7 +1,8 @@
 ﻿
+GetDictionary();
 function GetAllEntities() {
     jQuery.support.cors = true;
-    var Dict = $('#txtDictName').val();
+    var Dict = $('#sysDictList :selected').text();
     $.ajax({
         url: 'http://localhost:3761/api/' + Dict,
         type: 'GET',
@@ -18,7 +19,7 @@ function GetAllEntities() {
 function GetEntity() {
     jQuery.support.cors = true;
     var id = $('#txtId').val();
-    var Dict = $('#txtDictName').val();
+    var Dict = $('#sysDictList :selected').text();
     $.ajax({
         url: 'http://localhost:3761/api/' + Dict + '/' + id,
         type: 'GET',
@@ -34,13 +35,34 @@ function GetEntity() {
 
 function AddEntity() {
     jQuery.support.cors = true;
-    var Dict = $('#txtDictName').val();
-    var entities = {
-        Name: $('#txtaddName').val(),
-        LastUpdUS: $('#txtaddUs').val(),
-        LastUpdDT: $('#txtaddDtb').val(),
-        CountryID: $('#StudentDropDown').val()
-    };
+    var Dict = $('#sysDictList :selected').text();
+    if(Dict == 'Region')
+    {
+        var entities = {
+            Name: $('#txtaddName').val(),
+            CountryID: $('#linked1').val()
+        };
+    } else
+    if (Dict == 'District') {
+        var entities = {
+            Name: $('#txtaddName').val(),
+            RegionID: $('#linked1').val()
+        };
+    } else
+    if (Dict == 'City') {
+        var entities = {
+            Name: $('#txtaddName').val(),
+            RegionID: $('#linked3').val(),
+            DistrictID: $('#linked2').val(),
+            CityTypeID: $('#linked1').val()
+        };
+    } else
+    {
+        var entities = {
+            Name: $('#txtaddName').val()
+        };
+    }
+
 
     $.ajax({
         url: 'http://localhost:3761/api/' + Dict,
@@ -58,7 +80,7 @@ function AddEntity() {
 
 function DeleteEntity() {
     jQuery.support.cors = true;
-    var Dict = $('#txtDictName').val();
+    var Dict = $('#sysDictList :selected').text();
     var id = $('#txtdelId').val()
 
     $.ajax({
@@ -82,6 +104,7 @@ function WriteResponse(entities) {
     });
     strResult += "</table>";
     $("#divResult").html(strResult);
+    $('#lblResult').show();
 }
 
 function ShowEntity(entity) {
@@ -90,8 +113,96 @@ function ShowEntity(entity) {
         strResult += "<tr><td>" + entity.ID + "</td><td> " + entity.Name + "</td><td>" + entity.LastUpdUS + "</td><td>" + entity.LastUpdDT + "</td></tr>";
         strResult += "</table>";
         $("#divResult").html(strResult);
+        $('#lblResult').show();
     }
     else {
         $("#divResult").html("No Results To Display");
     }
 }
+
+function GetDictionary() {
+    jQuery.support.cors = true;
+    $.ajax({
+        type: "get",
+        url: "http://localhost:3761/api/Dictionary",
+        contentType: "application/json;charset=utf-8",
+        dataType: "json",
+        data: {},
+        success: function (result) {
+            $.each(result, function (i) {
+                $('#sysDictList').append($('<option></option>').val(result[i].ID).html(result[i].Name));
+            });
+        },
+        failure: function () {
+            alert("Error");
+        }
+    });
+}
+
+function GetRelatedEntities(name, comp) {
+    jQuery.support.cors = true;
+    $.ajax({
+        type: "get",
+        url: "http://localhost:3761/api/" + name,
+        contentType: "application/json;charset=utf-8",
+        dataType: "json",
+        data: {},
+        success: function (result) {
+            $(comp).empty();
+            $.each(result, function (i, result) {
+                $(comp).append($('<option></option>').val(result.ID).html(result.Name));
+            });
+        },
+        failure: function () {
+            alert("Error");
+        }
+    });
+}
+
+function GetStructur() {
+    jQuery.support.cors = true;
+    var name = $('#sysDictList :selected').text();
+    if (name == 'Region' || name == 'District') {
+        // where exists 1 related objects
+        if (name == 'Region')
+            name = 'Country';
+        else name = 'Region';
+        $('#trLinked1').show();
+        $('#lblLinked1').text(name + ':');
+        GetRelatedEntities(name, '#linked1');
+        $('#trLinked2').hide();
+        $('#lblLinked2').empty();
+        $('#linked2').empty();
+        $('#trLinked3').hide();
+        $('#lblLinked3').empty();
+        $('#linked3').empty();
+    }
+    else
+        if (name == 'City') {
+            // where exists 3 related objects
+            $('#trLinked1').show();
+            $('#lblLinked1').text("Type:");
+            GetRelatedEntities("CityType", '#linked1');
+
+            $('#trLinked2').show();
+            $('#lblLinked2').text("District:");
+            GetRelatedEntities("District", '#linked2');
+
+            $('#trLinked3').show();
+            $('#lblLinked3').text("Region:");
+            GetRelatedEntities("Region", '#linked3');
+        }
+        else {
+            // clear and hide
+            $('#trLinked1').hide();
+            $('#lblLinked1').empty();
+            $('#linked1').empty();
+            $('#trLinked2').hide();
+            $('#lblLinked2').empty();
+            $('#linked2').empty();
+            $('#trLinked3').hide();
+            $('#lblLinked3').empty();
+            $('#linked3').empty();
+        }
+
+};
