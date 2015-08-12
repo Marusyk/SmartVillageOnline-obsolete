3629efca2ab80f115668bb956a6b543ef46a70c2
Roma <romamarusyk@gmail.com>
2015-08-12 22:10:05 +0300

    Changed the template naming of FK from PrincipalTable_FKC_DependentTable to FK_PrincipalTable_DependentTable_ColumnName. Task #15

--- Domain/Migrations/CustomSqlGenerator.cs
+++ Domain/Migrations/CustomSqlGenerator.cs
@@ -8,16 +8,14 @@ namespace Domain
     {
         protected override void Generate(AddForeignKeyOperation addForeignKeyOperation)
         {
-            addForeignKeyOperation.Name = getFkName(addForeignKeyOperation.PrincipalTable,
-                addForeignKeyOperation.DependentTable, addForeignKeyOperation.DependentColumns.ToArray());
+            addForeignKeyOperation.Name = getFkName(addForeignKeyOperation.Name);
             base.Generate(addForeignKeyOperation);
         }
 
         protected override void Generate(DropForeignKeyOperation dropForeignKeyOperation)
         {
-            dropForeignKeyOperation.Name = getFkName(dropForeignKeyOperation.PrincipalTable,
-                dropForeignKeyOperation.DependentTable, dropForeignKeyOperation.DependentColumns.ToArray());
-            base.Generate(dropForeignKeyOperation);
+            dropForeignKeyOperation.Name = getFkName(dropForeignKeyOperation.Name);
+            base.Generate(dropForeignKeyOperation);         
         }
 
         protected override void Generate(CreateTableOperation table)
@@ -26,11 +24,9 @@ namespace Domain
             base.Generate(table);
         }
 
-        private static string getFkName(string primaryKeyTable, string foreignKeyTable, params string[] foreignTableFields)
-        {
-            primaryKeyTable = primaryKeyTable.Replace("dbo.", "");
-            foreignKeyTable = foreignKeyTable.Replace("dbo.", "");
-            return string.Format("{0}_FKC_{1}", primaryKeyTable, foreignKeyTable);
+        private static string getFkName(string foreignKeyName)
+        {  
+            return foreignKeyName.Replace("dbo.", "");
         }
 
         private static string getPkName(string primaryKeyTable)
