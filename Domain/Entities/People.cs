cabdfe4a86910c3295a73ddcbacc31fba5e42d0e
Roma <romamarusyk@gmail.com>
2015-08-31 22:20:22 +0300

    Added entity People. Task #16

---
+++ Domain/Entities/People.cs
@@ -0,0 +1,21 @@
+﻿using Domain.Abstract;
+using System;
+
+namespace Domain.Entities
+{
+    public class People : BaseEntity
+    {
+        public int PersonID { get; set; }
+
+        public int HouseID { get; set; }
+
+        public int PeopleNumber { get; set; }
+
+        public int? FamilyRelationsId { get; set; }
+
+        public DateTime? DateRegistration { get; set; }
+
+        public bool IsMain { get; set; }
+
+    }
+}
