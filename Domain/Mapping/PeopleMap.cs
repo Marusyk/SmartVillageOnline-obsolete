cd55e7efa615657cae8636d5964761f8d82cb221
Roma <romamarusyk@gmail.com>
2015-08-31 22:24:34 +0300

    Created mapping to table People. #16

---
+++ Domain/Mapping/PeopleMap.cs
@@ -0,0 +1,21 @@
+﻿using System.Data.Entity.ModelConfiguration;
+using Domain.Entities;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Domain.Mapping
+{
+    public class PeopleMap : EntityTypeConfiguration<People>
+    {
+        public PeopleMap()
+        {
+            HasKey(t => t.ID);
+            Property(t => t.ID).HasDatabaseGeneratedOption(DatabaseGeneratedOption.Identity);
+            Property(t => t.PersonID).IsRequired();
+            Property(t => t.HouseID).IsRequired();
+            Property(t => t.PeopleNumber).IsRequired();
+            Property(t => t.IsMain).IsRequired();            
+            Property(t => t.LastUpdUS).IsRequired().HasMaxLength(50);
+            ToTable("People");
+        }
+    }
+}
