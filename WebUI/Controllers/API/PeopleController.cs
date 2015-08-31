38accdd6f4747ac30924cd1e28a9e20622329888
Roma <romamarusyk@gmail.com>
2015-08-31 22:30:28 +0300

    Added controller People. Task #16

---
+++ WebUI/Controllers/API/PeopleController.cs
@@ -0,0 +1,24 @@
+﻿using Domain.Abstract;
+using Domain.Entities;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using System.Web.Http.OData;
+
+namespace WebUI.Controllers.API
+{
+    public class PeopleController : BaseApiController<People>
+    {
+        public PeopleController()
+            : base()
+        {
+        }
+
+        public PeopleController(IRepository<People> repository)
+            : base(repository)
+        {
+            this.repository = repository;
+        }
+    }
+}
