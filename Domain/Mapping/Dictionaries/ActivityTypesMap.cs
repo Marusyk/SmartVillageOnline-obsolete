319663016654058b3616c54aa8ea267b0eb634e9
Roma <romamarusyk@gmail.com>
2015-10-02 01:45:12 +0300

    Added ActivityTypes to domain and created controller ActivityTypes. #3

---
+++ Domain/Mapping/Dictionaries/ActivityTypesMap.cs
@@ -0,0 +1,19 @@
+﻿using System.Data.Entity.ModelConfiguration;
+using Domain.Entities;
+using System.ComponentModel.DataAnnotations.Schema;
+using Domain.Entities.Dictionaries;
+
+namespace Domain.Mapping.Dictionaries
+{
+    public class ActivityTypesMap : EntityTypeConfiguration<ActivityTypes>
+    {
+        public ActivityTypesMap()
+        {
+            HasKey(t => t.ID);
+            Property(t => t.ID).HasDatabaseGeneratedOption(DatabaseGeneratedOption.Identity);
+            Property(t => t.Name).IsRequired().HasMaxLength(50);
+            Property(t => t.LastUpdUS).IsRequired().HasMaxLength(50);
+            ToTable("ActivityTypes");
+        }
+    }
+}
---
+++ WebUI/Controllers/API/ActivityTypesController.cs
@@ -0,0 +1,19 @@
+﻿using Domain.Abstract;
+using Domain.Entities.Dictionaries;
+
+namespace WebUI.Controllers.API
+{
+    public class ActivityTypesController : BaseApiController<ActivityTypes>
+    {
+        public ActivityTypesController()
+            : base()
+        {
+        }
+
+        public ActivityTypesController(IRepository<ActivityTypes> repository)
+            : base(repository)
+        {
+            this.repository = repository;
+        }
+    }
+}
