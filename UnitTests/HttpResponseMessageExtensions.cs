bcd31d99e645707ba3848d18282e866b7e381d84
Roma <romamarusyk@gmail.com>
2015-11-27 23:17:44 +0200

    Create extension class for HttpResponseMessage . Task #17

---
+++ UnitTests/HttpResponseMessageExtensions.cs
@@ -0,0 +1,21 @@
+﻿using Domain.Abstract;
+using System.Linq;
+using System.Net.Http;
+
+namespace UnitTests
+{
+    public static class HttpResponseMessageExtensions
+    {
+        public static IQueryable<T> ContentToQueryable<T>(this HttpResponseMessage response) where T : BaseEntity
+        {
+            var objContent = response.Content as ObjectContent;
+            return objContent.Value as IQueryable<T>;
+        }
+
+        public static T ContentToEntity<T>(this HttpResponseMessage response) where T : BaseEntity
+        {
+            var objContent = response.Content as ObjectContent;
+            return objContent.Value as T;
+        }
+    }
+}
