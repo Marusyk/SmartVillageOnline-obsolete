73e0ce75b36b39cc1b3bb75188db471c08a811c1
Marusyk <romamarusyk@gmail.com>
2015-12-03 01:13:50 +0200

    Test

--- UnitTests/HouseTests.cs
+++ UnitTests/HouseTests.cs
@@ -14,140 +14,60 @@ using UnitTests.Infrastructure;
 namespace UnitTests
 {
     [TestClass]
-    public class HouseTests
+    public class HouseTests : BaseEntityUnitTest<House>
     {
-        #region private
-
-        private IRepository<House> CreateMockRepository()
+        public HouseTests()
         {
             // creating  fake Repository
-            var houses = new List<House>
-            {
-                new House {ID =1, HouseNr ="001", BuildNr ="1", Year = DateTime.Now.Year },
-                new House {ID =2, HouseNr ="002", BuildNr= "2", Year = DateTime.Now.Year },
-                new House {ID =3, HouseNr ="003", BuildNr ="3", Year = DateTime.Now.Year },
-                new House {ID =4, HouseNr ="004", BuildNr ="4", Year = DateTime.Now.Year },
-                new House {ID =5, HouseNr ="005", BuildNr ="5", Year = DateTime.Now.Year }
-            };
+            var persons = new List<House>
+             {
+                 new House {ID = 1, BuildNr = "01", HouseNr = "001" },
+                 new House {ID = 2, BuildNr = "02", HouseNr = "002" },
+                 new House {ID = 3, BuildNr = "03", HouseNr = "003" },
+                 new House {ID = 4, BuildNr = "04", HouseNr = "004" },
+                 new House {ID = 5, BuildNr = "05", HouseNr = "005" }
+             };
 
-            // configure the Mock Object
-            Mock<IRepository<House>> mock = new Mock<IRepository<House>>();
+            base.EntitiesList = persons;
 
-            mock.Setup(m => m.All).Returns(houses.AsQueryable());
+            // get Mock Repository from base class
+            var moq = base.CreateMockRepository();
 
-            mock.Setup(m => m.Add(It.IsAny<House>()))
-                .Callback<House>(c => houses.Add(c));
-
-            mock.Setup(m => m.Edit(It.IsAny<House>()))
-                .Callback<House>(c => houses[houses.IndexOf(c)] = c);
-
-            mock.Setup(m => m.GetById(It.IsAny<int>()))
-                .Returns<int>(c => houses.Find(f => f.ID == c));
-
-            mock.Setup(m => m.Delete(It.IsAny<House>()))
-                .Callback<House>(c => houses.Remove(c));
-
-            return mock.Object;
-        }
-
-        private HouseController ArrangeController()
-        {
-            // Get the mock Repository
-            var moq = CreateMockRepository();
+            // create Controller with Mock
             var controller = new HouseController(moq);
-            controller.Request = new HttpRequestMessage();
-            controller.Request.SetConfiguration(new HttpConfiguration());
-            return controller;
-        }
 
-        #endregion
+            // Init params of Controller
+            base.ArrangeController(controller);
+        }
 
         [TestMethod]
         public void House_Get_All()
         {
-            //Arrange
-            var target = ArrangeController();
-
-            //Action
-            var response = target.Get();
-            var result = response.ContentToQueryable<House>();
-
-            //Assert
-            Assert.AreEqual(5, result.Count());
+            base.GetAll();
         }
 
         [TestMethod]
         public void House_Get_By_Id()
         {
-            //Arrange
-            var target = ArrangeController();
-
-            //Action
-            var result = GetByID(target, 1);
-
-            //Assert
-            Assert.AreEqual(1, result.ID);
+            base.GetById();
         }
 
         [TestMethod]
         public void House_Can_Insert()
         {
-            //Arrange
-            var target = ArrangeController();
-
-            //Arrange - create a new country for insert
-            House newHouse = new House() { ID = 10, BuildNr = "010", HouseNr = "10" };
-
-            //Action
-            var resultInsert = target.Post(newHouse);
-            var resultSelect = GetByID(target, 10);
-            var resultTotalCount = target.Get().ContentToQueryable<House>();
-
-            //Assert
-            Assert.AreEqual(HttpStatusCode.Created, resultInsert.StatusCode);
-            Assert.AreEqual(10, resultSelect.ID);
-            Assert.AreEqual(6, resultTotalCount.Count());
-            Assert.AreEqual("010", resultSelect.BuildNr);
-            Assert.AreEqual("10", resultSelect.HouseNr);
+            base.Insert();
         }
 
         [TestMethod]
         public void House_Can_Edit()
         {
-            //Arrange
-            var target = ArrangeController();
-
-            //Action                     
-            var house = GetByID(target, 1);
-            house.HouseNr = "007";
-            var resultUpdate = target.Put(house);
-            var resultSelect = GetByID(target, 1);
-
-            //Assert
-            Assert.AreEqual(HttpStatusCode.OK, resultUpdate.StatusCode);
-            Assert.AreEqual(1, resultSelect.ID);
-            Assert.AreEqual("007", resultSelect.HouseNr);
+            base.Edit();
         }
 
         [TestMethod]
         public void House_Can_Remove()
         {
-            //Arrange
-            var target = ArrangeController();
-
-            //Action
-            var house = GetByID(target, 1);
-            var resultDelete = target.Delete(house.ID);
-            var resultSelect = target.Get().ContentToQueryable<House>();
-
-            //Assert
-            Assert.AreEqual(HttpStatusCode.OK, resultDelete.StatusCode);
-            Assert.AreEqual(4, resultSelect.Count());
-        }
-
-        private House GetByID(HouseController controller, int id)
-        {
-            return controller.GetById(id).ContentToEntity<House>();
+            base.Remove();
         }
     }
 }
--- UnitTests/PersonTests.cs
+++ UnitTests/PersonTests.cs
@@ -13,202 +13,78 @@ using UnitTests.Infrastructure;
 namespace UnitTests
 {
     [TestClass]
-    public class PersonTests
+    public class PersonTests : BaseEntityUnitTest<Person>
     {
-        #region private
-
-        private IRepository<Person> CreateMockRepository()
+        public PersonTests()
         {
             // creating  fake Repository
             var persons = new List<Person>
-            {
-                new Person {ID =1, FirstName = "Steven", LastName = "McConnell" },
-                new Person {ID =2, FirstName = "Bill", LastName = "Gates" },
-                new Person {ID =3, FirstName = "Larry", LastName = "Page" },
-                new Person {ID =4, FirstName = "Linus", LastName = "Torvalds" },
-                new Person {ID =5, FirstName = "Jeffrey", LastName = "Richter" }
-            };
+             {
+                 new Person {ID =1, FirstName = "Steven", LastName = "McConnell" },
+                 new Person {ID =2, FirstName = "Bill", LastName = "Gates" },
+                 new Person {ID =3, FirstName = "Larry", LastName = "Page" },
+                 new Person {ID =4, FirstName = "Linus", LastName = "Torvalds" },
+                 new Person {ID =5, FirstName = "Jeffrey", LastName = "Richter" }
+             };
 
-            // configure the Mock Object
-            Mock<IRepository<Person>> mock = new Mock<IRepository<Person>>();
+            base.EntitiesList = persons;
 
-            mock.Setup(m => m.GetAll()).Returns(persons.AsQueryable());
+            // get Mock Repository from base class
+            var moq = base.CreateMockRepository();
 
-            mock.Setup(m => m.Add(It.IsAny<Person>()))
-                .Callback<Person>(c => persons.Add(c));
-
-            mock.Setup(m => m.Edit(It.IsAny<Person>()))
-                .Callback<Person>(c => persons[persons.IndexOf(c)] = c);
-
-            mock.Setup(m => m.GetById(It.IsAny<int>()))
-                .Returns<int>(c => persons.Find(f => f.ID == c));
-
-            mock.Setup(m => m.Delete(It.IsAny<Person>()))
-                .Callback<Person>(c => persons.Remove(c));
-
-            return mock.Object;
-        }
-
-        private PersonController ArrangeController()
-        {
-            // Get the mock Repository
-            var moq = CreateMockRepository();
+            // create Controller with Mock
             var controller = new PersonController(moq);
-            controller.Request = new HttpRequestMessage();
-            controller.Request.SetConfiguration(new HttpConfiguration());
-            return controller;
-        }
 
-        #endregion
-
-        [TestMethod]
-        public void Person_Get_All()
-        {
-            //Arrange
-            var target = ArrangeController();
-
-            //Action
-            HttpResponseMessage response = target.Get();
-            var model = response.ContentToQueryable<Person>();
-
-            //Assert
-            Assert.AreEqual(5, model.Count());
+            // Init params of Controller
+            base.ArrangeController(controller);
         }
 
         [TestMethod]
-        public void Person_Get_By_Id()
-        {
-            //Arrange
-            var target = ArrangeController();
+         public void Person_Get_All()
+         {
+            base.GetAll();
+         }
 
-            //Action
-            HttpResponseMessage response = target.GetById(1);
-            var model = response.Content.ReadAsStringAsync().Result;// ReadAsAsync<Person>().Result;
+         [TestMethod]
+         public void Person_Get_By_Id()
+         {
+            base.GetById();
+         }
 
-            //Assert
-           // Assert.AreEqual(1, model.ID);
+         [TestMethod]
+         public void Person_Can_Insert()
+         {
+            base.Insert();                   
+         }
+
+         [TestMethod]
+         public void Person_Can_Edit()
+         {
+            base.Edit();
+         }
+
+         [TestMethod]
+         public void Person_Can_Remove()
+         {
+            base.Remove();
+         }
+
+         [TestMethod]
+         public void Person_Get_Paging_1_2()
+         {
+            base.GetAll(1, 2);
+         }
+
+         [TestMethod]
+         public void Person_Get_Paging_5_1()
+         {
+            base.GetAll(5, 1);
         }
 
-        [TestMethod]
-        public void Person_Can_Insert()
-        {
-            //Arrange
-            var target = ArrangeController();
-
-            //Arrange - create a new country for insert
-            var newPerson = new Person() { ID = 10, FirstName = "Larry", LastName = "Page" };
-
-            //Action
-            var resultInsert = target.Post(newPerson);
-            var resultSelect = target.GetById(10).Content.ReadAsStringAsync().Result;// ReadAsAsync<Person>().Result;
-            var resultTotalCount = target.Get().Content.ReadAsStringAsync().Result;// ReadAsAsync<Person>().Result;
-
-            //Assert
-            //Assert.AreEqual(HttpStatusCode.Created, resultInsert.StatusCode);
-            //Assert.AreEqual(10, resultSelect.ID);
-            //Assert.AreEqual(6, resultTotalCount.Count());
-            //Assert.AreEqual("Larry", resultSelect.FirstName);
-            //Assert.AreEqual("Page", resultSelect.LastName);                       
-        }
-
-        [TestMethod]
-        public void Person_Can_Edit()
-        {
-            //Arrange
-            var target = ArrangeController();
-
-            //Action                     
-            //var person = target.GetById(1).Content.ReadAsStringAsync().Result;// ReadAsAsync<Person>().Result;
-            //person.LastName = "McConnell";
-            //var resultUpdate = target.Put(person);
-            //var resultSelect = target.GetById(1).Content.ReadAsStringAsync().Result;// ReadAsAsync<Person>().Result;
-
-            ////Assert
-            //Assert.AreEqual(HttpStatusCode.OK, resultUpdate.StatusCode);
-            //Assert.AreEqual(1, resultSelect.ID);
-            //Assert.AreEqual("McConnell", resultSelect.LastName);
-        }
-
-        [TestMethod]
-        public void Person_Can_Remove()
-        {
-            //Arrange
-            var target = ArrangeController();
-
-            //Action
-            //var person = target.GetById(1).Content.ReadAsStringAsync().Result;// ReadAsAsync<Person>().Result;
-            //var resultDelete = target.Delete(person.ID);
-            //var resultSelect = target.Get().Content.ReadAsStringAsync().Result;// ReadAsAsync<Person>().Result;
-
-            ////Assert
-            //Assert.AreEqual(HttpStatusCode.OK, resultDelete.StatusCode);
-            //Assert.AreEqual(4, resultSelect.Count());
-        }
-
-        [TestMethod]
-        public void Person_Get_Paging_1_2()
-        {
-            //Arrange
-            var target = ArrangeController();
-
-            //Action
-            HttpResponseMessage response = target.Get(1, 2);
-            var result = response.Content.ReadAsStringAsync().Result;// ReadAsAsync<Person>().Result;
-
-            int pageNo = Convert.ToInt32(response.Headers.GetValues("X-Paging-PageNo").First());
-            int pageSize = Convert.ToInt32(response.Headers.GetValues("X-Paging-PageSize").First());
-            int pageCount = Convert.ToInt32(response.Headers.GetValues("X-Paging-PageCount").First());
-            int totalRecordCount = Convert.ToInt32(response.Headers.GetValues("X-Paging-TotalRecordCount").First());
-
-            //Assert
-            Assert.AreEqual(1, pageNo);
-            Assert.AreEqual(2, pageSize);
-            Assert.AreEqual(3, pageCount);
-            Assert.AreEqual(5, totalRecordCount);
-        }
-
-        [TestMethod]
-        public void Person_Get_Paging_5_1()
-        {
-            //Arrange
-            var target = ArrangeController();
-
-            //Action
-            HttpResponseMessage response = target.Get(5, 1);
-            //var result = response.Content.ReadAsAsync<IQueryable<Person>>().Result;
-
-            //int pageNo = Convert.ToInt32(response.Headers.GetValues("X-Paging-PageNo").First());
-            //int pageSize = Convert.ToInt32(response.Headers.GetValues("X-Paging-PageSize").First());
-            //int pageCount = Convert.ToInt32(response.Headers.GetValues("X-Paging-PageCount").First());
-            //int totalRecordCount = Convert.ToInt32(response.Headers.GetValues("X-Paging-TotalRecordCount").First());
-
-            ////Assert
-            //Assert.AreEqual(5, pageNo);
-            //Assert.AreEqual(1, pageSize);
-            //Assert.AreEqual(5, pageCount);
-            //Assert.AreEqual(5, totalRecordCount);
-        }
-
-        [TestMethod]
-        public void Person_Get_Paging_3_2()
-        {
-            //Arrange
-            var target = ArrangeController();
-
-            //Action
-            HttpResponseMessage response = target.Get(3, 2);
-            //var result = response.Content.ReadAsAsync<IQueryable<Person>>().Result;
-
-            //int pageNo = Convert.ToInt32(response.Headers.GetValues("X-Paging-PageNo").First());
-            //int pageSize = Convert.ToInt32(response.Headers.GetValues("X-Paging-PageSize").First());
-            //int pageCount = Convert.ToInt32(response.Headers.GetValues("X-Paging-PageCount").First());
-            //int totalRecordCount = Convert.ToInt32(response.Headers.GetValues("X-Paging-TotalRecordCount").First());
-
-            ////Assert
-            //Assert.AreEqual(3, pageNo);
-            //Assert.AreEqual(2, pageSize);
-            //Assert.AreEqual(3, pageCount);
-            //Assert.AreEqual(5, totalRecordCount);
+         [TestMethod]
+         public void Person_Get_Paging_3_2()
+         {
+            base.GetAll(3, 2);
         }
     }
 }
