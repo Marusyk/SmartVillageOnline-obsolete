7dc38bd575aecfe911ec05afb5dc9b9d2b0f6091
Marusyk <romamarusyk@gmail.com>
2015-12-03 02:53:45 +0200

    Added a new interface IBaseApiInterface and implementation for unit test that will use for entities. Task #35

---
+++ UnitTests/Infrastructure/BaseEntityUnitTest.cs
@@ -0,0 +1,182 @@
+﻿using Domain.Abstract;
+using Domain.Concrete;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Moq;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+using System.Web.Http;
+using WebUI.Infrastructure;
+
+namespace UnitTests.Infrastructure
+{
+    public class BaseEntityUnitTest<T> : IBaseEntityUnitTest<T> where T : BaseEntity, new()
+    {
+        protected IBaseApiInterface<T> Controller;
+        protected List<T> EntitiesList;
+
+
+        private static List<T> CreateDefaultList()
+        {
+            return new List<T>
+            {
+                new T {ID = 1, LastUpdUS = "Entity1" },
+                new T {ID = 2, LastUpdUS = "Entity2" },
+                new T {ID = 3, LastUpdUS = "Entity3" },
+                new T {ID = 4, LastUpdUS = "Entity4" },
+                new T {ID = 5, LastUpdUS = "Entity5" }
+            };
+        }
+
+        private List<T> GetEntitiesList()
+        {
+            return EntitiesList != null ? EntitiesList : CreateDefaultList();
+        }
+
+        public virtual IRepository<T> CreateMockRepository()
+        {
+            // configure the standart Mock Object for CRUD operations
+            Mock<IRepository<T>> mock = new Mock<IRepository<T>>();
+
+            // GetAll()
+            mock.Setup(m => m.GetAll()).Returns(EntitiesList.AsQueryable());
+
+            //GetById(int id)
+            mock.Setup(m => m.GetById(It.IsAny<int>()))
+               .Returns<int>(c => EntitiesList.Find(f => f.ID == c));
+
+            // FindBy()
+            //mock.Setup(m => m.FindBy(f => f.LastUpdUS == It.IsAny<string>()))
+            //    .Returns(EntitiesList.AsQueryable());
+
+            // Add()
+            mock.Setup(m => m.Add(It.IsAny<T>()))
+                .Callback<T>(c => EntitiesList.Add(c));
+
+            // Edit
+            mock.Setup(m => m.Edit(It.IsAny<T>()))
+                .Callback<T>(c => EntitiesList[EntitiesList.IndexOf(c)] = c);
+
+            // Delete
+            mock.Setup(m => m.Delete(It.IsAny<T>()))
+                .Callback<T>(c => EntitiesList.Remove(c));
+
+            // Paginate
+            // TODO: 
+
+            return mock.Object;
+        }
+
+        public virtual void ArrangeController(IBaseApiInterface<T> controller)
+        {
+            Controller = controller;
+            ((ApiController)controller).Request = new HttpRequestMessage();
+            ((ApiController)controller).Request.SetConfiguration(new HttpConfiguration());
+        }
+
+        public virtual void GetAll()
+        {
+            //Action
+            var response = Controller.Get();
+            var result = response.ContentToQueryable<T>();
+            //Assert
+            Assert.AreEqual(5, result.Count());
+        }
+
+        public virtual void GetAll(int pageNo, int pageSize)
+        {
+            //Arrange
+            /*int pageCount = EntitiesList.Count() > 0 ? (int)Math.Ceiling(EntitiesList.Count() / (double)pageSize) : 0;
+
+            if (pageSize > 0 & pageSize > 0)
+            {
+                //Action
+                HttpResponseMessage response = Controller.Get(pageNo, pageSize);
+                var result = response.Content.ReadAsStringAsync().Result;// ReadAsAsync<IQueryable<T>>().Result;
+
+                int _pageNo = Convert.ToInt32(response.Headers.GetValues("X-Paging-PageNo").First());
+                int _pageSize = Convert.ToInt32(response.Headers.GetValues("X-Paging-PageSize").First());
+                int _pageCount = Convert.ToInt32(response.Headers.GetValues("X-Paging-PageCount").First());
+                int _totalRecordCount = Convert.ToInt32(response.Headers.GetValues("X-Paging-TotalRecordCount").First());
+
+                //Assert
+                Assert.AreEqual(pageNo, _pageNo);
+                Assert.AreEqual(pageSize, _pageSize);
+                Assert.AreEqual(pageCount, _pageCount);
+                Assert.AreEqual(EntitiesList.Count(), _totalRecordCount);
+                
+            }*/
+            Assert.AreEqual(0, 0);
+        }
+
+        public virtual void GetById()
+        {
+            // Action
+            var result = GetById(1);
+
+            // Accert
+            Assert.IsNotNull(result);
+        }
+
+        public virtual void Insert()
+        {
+            // Arrange
+            var entity = new T() { ID = 10, LastUpdUS = "TEST" };
+
+            // Act
+            var resultInsert = Controller.Post(entity);
+            var resultSelect = GetById(10);
+            var resultTotalCount = Controller.Get().ContentToQueryable<T>();
+
+            // Assert
+            Assert.AreEqual(HttpStatusCode.Created, resultInsert.StatusCode);
+            Assert.AreEqual(10, resultSelect.ID);
+            Assert.AreEqual(6, resultTotalCount.Count());
+            Assert.AreEqual("TEST", resultSelect.LastUpdUS);
+        }
+
+        public void Edit()
+        {
+            // Arrange
+            var entity = GetById(1);
+            entity.LastUpdUS = "TEST";
+
+            //Action                                 
+            var resultUpdate = Controller.Put(entity);
+            var resultSelect = GetById(1);
+
+            //Assert
+            Assert.AreEqual(HttpStatusCode.OK, resultUpdate.StatusCode);
+            Assert.AreEqual(1, resultSelect.ID);
+            Assert.AreEqual("TEST", resultSelect.LastUpdUS);
+        }
+
+        public void Remove()
+        {
+            // Arrange
+            var entity = GetById(1);
+
+            //Action
+            var resultDelete = Controller.Delete(entity.ID);
+            var resultSelect = Controller.Get().ContentToQueryable<T>();
+
+            //Assert
+            Assert.AreEqual(HttpStatusCode.OK, resultDelete.StatusCode);
+            Assert.AreEqual(4, resultSelect.Count());
+        }
+
+        private T GetById(int id)
+        {
+            return Controller.GetById(id).ContentToEntity<T>();
+        }
+
+        public void FindBy()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ UnitTests/Infrastructure/IBaseEntityUnitTest.cs
@@ -0,0 +1,19 @@
+﻿using Domain.Abstract;
+using WebUI.Infrastructure;
+
+namespace UnitTests.Infrastructure
+{
+    interface IBaseEntityUnitTest<T> where T : BaseEntity
+    {
+        IRepository<T> CreateMockRepository();
+        void ArrangeController(IBaseApiInterface<T> controller);
+
+        void GetAll();
+        void GetById();
+        void GetAll(int pageNo, int pageSize);
+        void Insert();
+        void Edit();
+        void Remove();
+        void FindBy();
+    }
+}
