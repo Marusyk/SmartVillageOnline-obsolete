d62bd7591c647c259306301610880a6f042ed144
Marusyk <romamarusyk@gmail.com>
2015-12-11 21:47:50 +0200

    Added OData package. Task #18

--- WebUI/Infrastructure/BaseApiController.cs
+++ WebUI/Infrastructure/BaseApiController.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Net;
 using System.Net.Http;
 using System.Web.Http;
+using System.Web.OData;
 using Domain;
 using Domain.Abstract;
 
@@ -78,7 +79,7 @@ namespace WebUI.Infrastructure
         #endregion
 
         #region GET
-
+        [EnableQuery]
         public virtual HttpResponseMessage Get()
         {
             var entity = Repository.GetAll();
